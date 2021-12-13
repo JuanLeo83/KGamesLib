874fe0a0818153c26166530996c43b621b52dba0
Juan G Pérez Leo <jgperez@atsistemas.com>
2021-12-13 11:21:34 +0100

    KGames IntelliJ scope created and Window refactor to clean code

--- src/main/kotlin/window/Window.kt
+++ src/main/kotlin/window/Window.kt
@@ -31,9 +31,18 @@ class Window(
     }
 
     private fun initialize() {
-        errorCallback = glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
-        check(glfwInit() == GLFW_TRUE) { "Unable to initialize GLFW window" }
+        setErrorCallback()
+        check(glfwInit() == GLFW_TRUE) { "Unable to initialize GLFW" }
+        createWindow()
+        setKeyCallback()
+        glfwShowWindow(window!!)
+    }
 
+    private fun setErrorCallback() {
+        errorCallback = glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
+    }
+
+    private fun createWindow() {
         glfwDefaultWindowHints()
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
@@ -41,6 +50,21 @@ class Window(
         window = glfwCreateWindow(windowDimensions.width, windowDimensions.height, windowTitle, NULL, NULL)
         check(window != NULL) { "Failed to create the GLFW window" }
 
+        setWindowPositionInScreen()
+        glfwMakeContextCurrent(window!!)
+        glfwSwapInterval(1)
+    }
+
+    private fun setWindowPositionInScreen() {
+        val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+        glfwSetWindowPos(
+            window!!,
+            (vidmode.width() - windowDimensions.width) / 2,
+            (vidmode.height() - windowDimensions.height) / 2
+        )
+    }
+
+    private fun setKeyCallback() {
         keyCallback = glfwSetKeyCallback(window!!, object : GLFWKeyCallback() {
             override fun invoke(
                 window: kotlin.Long,
@@ -49,22 +73,11 @@ class Window(
                 action: kotlin.Int,
                 mods: kotlin.Int
             ) {
-
                 if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
                     glfwSetWindowShouldClose(window, GLFW_TRUE)
                 }
             }
         })
-
-        val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
-        glfwSetWindowPos(
-            window!!,
-            (vidmode.width() - windowDimensions.width) / 2,
-            (vidmode.height() - windowDimensions.height) / 2
-        )
-        glfwMakeContextCurrent(window!!)
-        glfwSwapInterval(1)
-        glfwShowWindow(window!!)
     }
 
     private fun loop() {

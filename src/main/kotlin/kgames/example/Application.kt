23253af295f6938fdb6a2ac8304b2e6f8eb13ede
Juan Leo <juanleodev@gmail.com>
2023-08-10 08:34:18 +0200

    Experimentos con glfw

--- src/main/kotlin/kgames/example/Application.kt
+++ src/main/kotlin/kgames/example/Application.kt
@@ -1,33 +1,77 @@
-import kgames.core.file.KFile
-import kgames.core.file.KFolder
-import kgames.core.initGame
-import kgames.example.GameExample
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.NULL
 
-fun main(args: Array<String>) {
-    initGame(GameExample())
-//    testFileManager()
-}
 
-fun testFileManager() {
+fun main() {
+    GLFWErrorCallback.createPrint(System.err).set()
 
-//    val kFile = KFile("aResourceFile", "txt")
-//    val content = kFile.loadAsString()
-//    println("Original content: $content")
-//
-//    kFile.write("content of file".toByteArray())
-//    println("Result: ${String(kFile.load())}")
+    if (!glfwInit()) throw IllegalStateException("GLFW can't be initialised")
 
-//    val anotherFile = KFile("another","file")
-//    anotherFile.create()
-//    anotherFile.delete()
+    println("Version: $GLFW_VERSION_MAJOR.$GLFW_VERSION_MINOR.$GLFW_VERSION_REVISION")
+    println("Version: ${glfwGetVersionString()}")
 
-    val folder = KFolder("aFolder")
-    folder.create()
-    val file = KFile(folder, "a.file")
-    file.create()
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+//    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE)
 
-    val filesInFolder = folder.listFiles()
-    filesInFolder.forEach {
-        println(it.getAbsolute())
+
+    val monitor = glfwGetPrimaryMonitor()
+    val mode = glfwGetVideoMode(monitor)
+
+    val width = mode?.width() ?: 800
+    val height = mode?.height() ?: 600
+
+    val window = glfwCreateWindow(800, 600, "Holis", NULL, NULL)
+    if (window == NULL) throw RuntimeException("Failed to create the GLFW window")
+
+    glfwSetKeyCallback(window) { window: Long, key: Int, scancode: Int, action: Int, mods: Int ->
+
+//        println("time: ${glfwGetTime()} key: $key, action: $action")
+
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+            glfwSetWindowShouldClose(window, true) // We will detect this in the rendering loop
+        if (key == GLFW_KEY_1 && action == GLFW_RELEASE)
+            glfwSetWindowSize(window, 1920, 1080)
+        if (key == GLFW_KEY_2 && action == GLFW_RELEASE)
+            glfwSetWindowSize(window, 800, 600)
+        if (key == GLFW_KEY_0 && action == GLFW_RELEASE)
+            glfwIconifyWindow(window)
+        if (key == GLFW_KEY_9 && action == GLFW_RELEASE)
+            glfwRequestWindowAttention(window)
     }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE)
+
+//    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN)
+
+    glfwSetWindowSizeLimits(window, 300, 300, GLFW_DONT_CARE, GLFW_DONT_CARE)
+
+//    glfwSetFramebufferSizeCallback(window) { window: Long, width: Int, height: Int ->
+//        println("Size: $width - $height")
+//    }
+
+    glfwSetWindowPosCallback(window) { window, x, y ->
+        println("($x, $y)")
+    }
+
+    glfwMakeContextCurrent(window)
+    glfwSwapInterval(1)
+    glfwShowWindow(window)
+
+    GL.createCapabilities()
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT) // clear the framebuffer
+        glfwSwapBuffers(window) // swap the color buffers
+        glfwPollEvents()
+    }
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
 }
+
